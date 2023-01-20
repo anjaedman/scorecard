79152d285d1249a1905589749a1644c0965d77ce
anjaedman <113342171+anjaedman@users.noreply.github.com>
2023-01-20 11:40:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+let saveHome = document.getElementById("save-home")
+let saveGuest = document.getElementById("save-guest")
+
+let countHome = 0
+let countGuest = 0 
+
+
+function home1() {
+    countHome += 1
+    saveHome.textContent = countHome
+}
+
+function home2()  {
+    countHome += 2
+    saveHome.textContent = countHome
+}
+function home3()  {
+    countHome += 3
+    saveHome.textContent = countHome
+}
+
+function guest1()  {
+    countGuest += 1
+    saveGuest.textContent = countGuest
+}
+function guest2() {
+    countGuest += 2
+    saveGuest.textContent = countGuest
+}
+function guest3() {
+    countGuest += 3
+    saveGuest.textContent = countGuest
+}
+
+
+/*
+? varför
+! därför
+& å här
+* jooooo
+^ näääääääää
+~ naturligtvis
+*/
+
+// & yes
+// ? vill jaaa
+// * roooligt
+// ^ bäst
+// ~~ lärorikt
+// ! ABSOLUT
